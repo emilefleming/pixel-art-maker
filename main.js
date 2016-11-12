3d17703b60b4d23413d0494e6df0423baa76ac6b
Emile Mirth Fleming <emile.fleming@gmail.com>
2016-11-11 16:46:40 -0800

    Make app work, add color picker

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 (function() {
   'use strict';
-  let color = 'red';
+  let color = '#EF382B';
+  let activeTool = 'draw';
 // Build a grid of pixels
   const makeBoard = function(height, width) {
     const artBoard = document.createElement('div');
-    artBoard.className = 'artBoard';
+    artBoard.classList.add('borders', 'artBoard')
     for (let i = 0; i < height; i++) {
       const row = document.createElement('div');
       for (let i = 0; i < width; i++) {
@@ -18,14 +19,46 @@
   }
   document.querySelector('main').appendChild(makeBoard(100, 100));
 
+
+  const setColor = function() {
+    color = document.getElementById("colorPicker").value;
+  }
 // Recolor pixels
-  const recolor = function(event) {
-    const pixel = event.target;
-    if (pixel.className === 'artBoard' || pixel.className === 'row') {
-      return;
-    }
+  const recolor = function(pixel) {
     pixel.style.backgroundColor = color;
     pixel.style.border = "0";
   };
-  document.querySelector('.artBoard').addEventListener('click', recolor);
+
+// Set tool
+  const setTool = function(tool) {
+    activeTool = tool;
+  }
+
+// Do action with tool
+  const useTool = function(event) {
+    const target = event.target;
+    if (target.className === 'artBoard' || target.className === 'row') {
+      return;
+    }
+    if (activeTool === 'draw') {
+      recolor(target);
+    } else if (activeTool === 'erase') {
+      erase(target);
+    }
+  }
+
+// Color picker functions
+  const openPicker = function() {
+    const elem = document.getElementById('colorPicker');
+    if(elem) {
+        elem.click();
+    }
+  }
+
+
+// Add event listeners
+  document.querySelector('.artBoard').addEventListener('click', useTool);
+  document.getElementById('drawTool').addEventListener('click', function(){setTool('draw')});
+  document.getElementById('colorTool').addEventListener('click', openPicker);
+  document.querySelector('#colorPicker').addEventListener('input', setColor)
 })();
